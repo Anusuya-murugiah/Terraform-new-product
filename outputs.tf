fd0834da397d0dfd9f4832ebcca5fa0327becaf0
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2026-03-26 17:15:33 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = module.ec2_instance.id
-}
-
 output "instance_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = module.ec2_instance.public_ip
